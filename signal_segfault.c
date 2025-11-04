0bbfd8640ca2fcb05f749c117d56404e86949285
Ashlyn <dederta@msoe.edu>
2025-11-04 02:25:41 -0600

    Updated signal_segfault.c to print a message upon catching a segmentation fault.

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,29 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Ashlyn Dedert
  * 
  * Brief summary of modifications:
+ * - added a signal handler for SIGSEGV that prints a message when a segmentation fault occurs
+ * - demonstrates what happens when you don't exit after catching.
  */
 
-
+#include <signal.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+void segfault_handler(int signal)
+{
+    if (signal == SIGSEGV)
+    {
+        printf("SIGSEGV caught: null pointer dereference\n");
+    }
+}
+
 int main (int argc, char* argv[]) {
+    // Register signal handler
+    signal(SIGSEGV, segfault_handler);
+
     // Declare a null pointer
     int* i = NULL;
 

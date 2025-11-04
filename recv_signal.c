81a1d46282fb277ffee60496b020e073ea07c9d4
Ashlyn <dederta@msoe.edu>
2025-11-04 08:01:42 -0600

    Added sending and receiving signals

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,53 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Ashlyn Dedert
+ * CPE 2600 111
+ * Lab 10 - Signals
  * 
  * Brief summary of program:
+ * Uses sigaction with SA_SIGINFO to handle SIGUSR1 signals to print a integer
+ * (sival_int) sent with sigqueue from another process.
+ * 
+ * To test:
+ * 1. Run this program ./recv_signal
+ * 2. Note the printed PID and run send_signal in another terminal.
  */
+
+#define _GNU_SOURCE
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handler(int signal, siginfo_t *info, void *context)
+{
+    if (signal == SIGUSR1)
+    {
+        printf("Received SIGUSR1 with value: %d\n", info->si_value.sival_int);
+        exit(0);
+    }
+}
+
+int main()
+{
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        return 1;
+    }
+
+    printf("Receiver PID: %d\n", getpid());
+    printf("Waiting for SIGUSR1 with data...\n");
+
+    while (1)
+    {
+        pause(); // wait for signals
+    }
+
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,45 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Ashlyn Dedert
+ * CPE 2600 111
+ * Lab 10 - Signals
  * 
  * Brief summary of program:
+ * Sends a SIGUSR1 signal with a random integer to another process using sigqueue
+ * The PID of the receiving process is provided as a command line argument.
+ * 
+ * Usage:
+ *     ./send_signal <PID>
  */
+
+ #include <signal.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <time.h>
+
+ int main(int argc, char* argv[])
+ {
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <PID>\n", argv[0]);
+        return 1;
+    }
+
+    pid_t pid = atoi(argv[1]);
+    srand(time(NULL));
+
+    int value = rand() % 100;
+    printf("Sending SIGUSR1 with value: %d to PID: %d\n", value, pid);
+
+    union sigval sig_data;
+    sig_data.sival_int = value;
+
+    if (sigqueue(pid, SIGUSR1, sig_data) == -1)
+    {
+        perror("sigqueue");
+        return 1;
+    }
+
+    printf("Signal sent successfully. \n");
+    return 0;
+ }

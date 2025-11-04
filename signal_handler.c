f8f3f1703db009527ad9d393edff8fcf5f38dc7a
Ashlyn <dederta@msoe.edu>
2025-11-04 00:53:27 -0600

    Filled in signal_handler and signal_sigaction

--- signal_handler.c
+++ signal_handler.c
@@ -4,9 +4,12 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Ashlyn Dedert
  * 
  * Brief summary of modifications:
+ *  - removed the exit() call from the signal handler.
+ *  - added a global flag so main can exit gracefully after receiving SIGINT
+ *  - added PID printout for testing with the kill command.
  */
 
 
@@ -15,15 +18,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+volatile sig_atomic_t signal_received = 0;
+
 /**
- * @brief Signal handler for SIGINT - prints a message and exits
+ * @brief Signal handler for SIGINT - prints a message and sets a flag
  */
-void handle_signal() {
-    printf("Received a signal\n");
-    exit(1);
+void handle_signal(int signal) {
+    printf("\nReceived SIGINT, signal number: %d\n", signal);
+    signal_received = 1; // mark SIGINT as received
 }
 
 int main() {
+
+    printf("PID %d\n", getpid());
+    printf("Waiting for SIGINT (Press Ctrl+C)...\n");
 
     // Register for the signal
     signal(SIGINT, handle_signal);
@@ -32,6 +40,10 @@ int main() {
     while(1) {
         printf("Sleeping\n");
         sleep(1);
+        if (signal_received) {
+            printf("Exiting gracefully after receiving SIGINT.\n");
+            break; // exit the loop and terminate the program
+        }
     }
 
     return 0;
--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,44 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_sigaction.c
+ * Modified by: Ashlyn Dedert
+ * CPE 2600 111
+ * Lab 10 - Signals
  * 
  * Brief summary of program:
+ * 
  */
 
+#define _GNU_SOURCE
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+void handler(int signal, siginfo_t *info, void *context) 
+{
+    if (signal == SIGUSR1) 
+    {
+        printf("Received SIGUSR1 from PID: %d\n", info->si_pid);
+    }
+}
+
+int main()
+{
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO; // important to get siginfo_t populated
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) 
+    {
+        perror("sigaction");
+        return 1;
+    }
+    printf("Process PID: %d\n", getpid());
+    printf("Waiting for SIGUSR1 signal...\n");
+
+    while (1)
+    {
+        pause(); // wait for signals
+    }
+
+    return 0;
+}
